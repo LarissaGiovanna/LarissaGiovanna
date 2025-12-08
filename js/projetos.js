b7e22a03f22d9f3f30ccb4ee10f0421e842e2a4d
LarissaGiovanna <lgiova544@gmail.com>
2025-12-08 18:16:02 -0300

    feat: add projetos

---
+++ js/projetos.js
@@ -0,0 +1,26 @@
+const sectAcademicos = document.getElementById('academicos');
+const sectPessoais = document.getElementById('pessoais');
+const projetosAcademicos = document.getElementById('projetos-academicos');
+const projetosPessoais = document.getElementById('projetos-pessoais');
+
+sectAcademicos.addEventListener('click', () => {
+    if (projetosAcademicos.style.display === 'block') {
+        projetosAcademicos.style.display = 'none';
+        projetosPessoais.style.display = 'block';
+    }
+    else{
+        projetosAcademicos.style.display = 'block';
+        projetosPessoais.style.display = 'none';
+    }
+});
+
+sectPessoais.addEventListener('click', () => {
+    if (projetosAcademicos.style.display === 'block') {
+        projetosAcademicos.style.display = 'none';
+        projetosPessoais.style.display = 'block';
+    }
+    else{
+        projetosPessoais.style.display = 'block';
+        projetosAcademicos.style.display = 'none';
+    }
+});
